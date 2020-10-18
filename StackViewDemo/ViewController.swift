e9c1009f57473937660857f0d31c8d46cdf31b19
Headshake <beatricelim188@yahoo.com>
2020-10-18 14:34:04 +0800

    Initial Commit

---
+++ StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by WiseLab-HP on 2020/10/18.
+//  Copyright © 2020 WiseLab-HP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
